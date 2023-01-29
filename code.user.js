bb1eaf3b67b30f78d6e024cd5ef829bbe6079e6c
Chris <44344493+ChrisDaDerp@users.noreply.github.com>
2023-01-28 23:33:51 -0500

    Create code.user.js

---
+++ code.user.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name         Air Balloon Injector
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Shows Air Balloon PNG when "Balloon" is detected in a Pokémon Showdown battle.
+// @author       ChrisDaDerp
+// @match        *://play.pokemonshowdown.com/battle-*
+// @icon         https://github.com/PokeAPI/sprites/blob/master/sprites/items/air-balloon.png?raw=true
+// @grant        none
+// ==/UserScript==
+
+const airBalloonImg = new Image();
+airBalloonImg.src = 'https://github.com/PokeAPI/sprites/blob/master/sprites/items/air-balloon.png?raw=true';
+const airBalloon1 = document.createElement('img');
+const airBalloon2 = document.createElement('img');
+// set the src of the img leftElements to the Air Balloon image
+airBalloon1.src = airBalloonImg.src;
+// set the width of the img element
+airBalloon1.width = 50;
+// set the src of the img rightElements to the Air Balloon image
+airBalloon2.src = airBalloonImg.src;
+// set the width of the img rightElements
+airBalloon2.width = 50;
+
+// Checks left for Balloon status
+function checkLeft() {
+    const leftElements = document.querySelectorAll('div.statbar.lstatbar.leftstatbar');
+
+    // Loop through the leftElements
+    for (let i = 0; i < leftElements.length; i++) {
+        // Check if the current leftElement's innerText contains the string "Balloon"
+        if (leftElements[i].innerText.includes('Balloon')) {
+            leftElements[i].appendChild(airBalloon1);
+        }
+        // Remove balloon if no longer active
+        else if (!leftElements[i].innerText.includes('Balloon')) {
+            leftElements[i].removeChild(airBalloon1);
+        }
+    }
+}
+
+// Checks right for Balloon status
+function checkRight() {
+    const rightElements = document.querySelectorAll('div.statbar.rstatbar.leftstatbar');
+
+    // Loop through the rightElements
+    for (let i = 0; i < rightElements.length; i++) {
+        // Check if the current rightElements's innerText contains the string "Balloon"
+        if (rightElements[i].innerText.includes('Balloon')) {
+            rightElements[i].appendChild(airBalloon2);
+        }
+        // Remove balloon if no longer active
+        else if (!rightElements[i].innerText.includes('Balloon')) {
+            rightElements[i].removeChild(airBalloon2);
+        }
+    }
+}
+
+// Checks left and right sides for balloons
+function checkForBalloon() {
+    try {
+        checkLeft()
+    } catch (error) {
+        console.error(error);
+    }
+    try {
+    checkRight()
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+setInterval(checkForBalloon, 1000);
