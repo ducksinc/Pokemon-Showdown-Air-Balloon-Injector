12b438c330595c407556c2b7b34baefb541483a2
Chris <44344493+ChrisDaDerp@users.noreply.github.com>
2023-01-30 20:16:41 -0500

    Combined checkLeft() and checkRight() into checkBalloon()

--- code.user.js
+++ code.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Air Balloon Injector
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Shows Air Balloon PNG when "Balloon" is detected in a Pokémon Showdown battle.
 // @author       ChrisDaDerp
 // @match        *://play.pokemonshowdown.com/battle-*
@@ -11,71 +11,48 @@
 // @grant        none
 // ==/UserScript==
 
+// Air Balloon image setup.
 const airBalloonImg = new Image();
 airBalloonImg.src = 'https://github.com/PokeAPI/sprites/blob/master/sprites/items/air-balloon.png?raw=true';
+
+// Left balloon setup.
 const airBalloon1 = document.createElement('img');
-const airBalloon2 = document.createElement('img');
-// set the src of the img leftElements to the Air Balloon image
 airBalloon1.src = airBalloonImg.src;
-// set the width of the img element
 airBalloon1.width = 50;
 let balloon1 = false;
-// set the src of the img rightElements to the Air Balloon image
+
+// Right balloon setup.
+const airBalloon2 = document.createElement('img');
 airBalloon2.src = airBalloonImg.src;
-// set the width of the img rightElements
 airBalloon2.width = 50;
 let balloon2 = false;
 
-// Checks left for Balloon status
-function checkLeft() {
-    const leftElements = document.querySelectorAll('div.statbar.lstatbar.leftstatbar');
-
-    // Loop through the leftElements
-    for (let i = 0; i < leftElements.length; i++) {
-        // Check if the current leftElement's innerText contains the string "Balloon"
-        if (leftElements[i].innerText.includes('Balloon')) {
-            leftElements[i].appendChild(airBalloon1);
-            balloon1 = true;
+// Checks for balloon status and then either adds or removes balloon from the side of the field.
+function checkBalloon(pokeElements, balloonStatus, airBalloon) {
+    // Loop through the pokeElements.
+    for (let i = 0; i < pokeElements.length; i++) {
+        // Check if the current pokeElements's innerText contains the string "Balloon". Updates balloon status if present.
+        if (pokeElements[i].innerText.includes('Balloon')) {
+            pokeElements[i].appendChild(airBalloon);
+            balloonStatus = true;
+            console.log(pokeElements[i]);
         }
-        // Remove balloon if no longer active
-        else if (!leftElements[i].innerText.includes('Balloon') && balloon1) {
-            leftElements[i].removeChild(airBalloon1);
-            balloon1 = false;
-        }
-    }
-}
-
-// Checks right for Balloon status
-function checkRight() {
-    const rightElements = document.querySelectorAll('div.statbar.rstatbar.leftstatbar');
-
-    // Loop through the rightElements
-    for (let i = 0; i < rightElements.length; i++) {
-        // Check if the current rightElements's innerText contains the string "Balloon"
-        if (rightElements[i].innerText.includes('Balloon')) {
-            rightElements[i].appendChild(airBalloon2);
-            balloon2 = true;
-        }
-        // Remove balloon if no longer active
-        else if (!rightElements[i].innerText.includes('Balloon') && balloon2) {
-            rightElements[i].removeChild(airBalloon2);
-            balloon2 = false;
+        // Remove balloon if no longer active. Updates balloon status when removed.
+        else if (!(pokeElements[i].innerText.includes('Balloon')) && balloonStatus) {
+            pokeElements[i].removeChild(airBalloon);
+            balloonStatus = false;
         }
     }
 }
 
-// Checks left and right sides for balloons
+// Checks left and right sides for balloons.
 function checkForBalloon() {
-    try {
-        checkLeft()
-    } catch (error) {
-        console.error(error);
-    }
-    try {
-    checkRight()
-    } catch (error) {
-        console.error(error);
-    }
+    const leftElements = document.querySelectorAll('div.statbar.lstatbar.leftstatbar');
+    checkBalloon(leftElements, balloon1, airBalloon1);
+
+    const rightElements = document.querySelectorAll('div.statbar.rstatbar.leftstatbar');
+    checkBalloon(rightElements, balloon2, airBalloon2);
 }
 
+// Runs every second.
 setInterval(checkForBalloon, 1000);
